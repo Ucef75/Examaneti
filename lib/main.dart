dac75820cb0ef01cc74c5cacb0089129b8ced88e
ucef75 <benabidyoussef75@gmail.com>
2025-05-01 08:23:13 +0100

    Fixed bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,28 @@
-import 'package:app/Pages/Home.dart';
+import 'Pages/Home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/add_exam_screen.dart';
+import 'package:flutter/foundation.dart' as Foundation; // For checking web platform
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
+  
+  // Firebase initialization for Web
+  if (Foundation.kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
         apiKey: "AIzaSyByQ3c2lU6hN0WyJufFOz9M5BEa-wC9VTM",
         authDomain: "appflutter-2a1e7.firebaseapp.com",
         projectId: "appflutter-2a1e7",
-        storageBucket: "appflutter-2a1e7.appspot.com", // I also fixed a typo here (was wrong in your storageBucket)
+        storageBucket: "appflutter-2a1e7.appspot.com", 
         messagingSenderId: "514872078538",
         appId: "1:514872078538:web:ac0dd6bd44a31f75070433",
         databaseURL: 'https://myapp-1234.firebaseio.com',
       ),
     );
   } else {
-    await Firebase.initializeApp(); // <-- Add await here!
+    // Firebase initialization for Android/iOS
+    await Firebase.initializeApp();
   }
 
   runApp(const MyApp());
@@ -32,10 +36,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Home Page',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: AddExamScreen(), // Start with the Add Exam screen
+      home: const AddExamScreen(), // Start with the Add Exam screen
     );
   }
 }
